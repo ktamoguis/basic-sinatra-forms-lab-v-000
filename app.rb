b0d3f693e0165e02bff70d50a316c2524d215b3e
Karl Amoguis <ktamoguis@gmail.com>
2018-06-17 09:19:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team=params[:team]
+    @team=params[:team_name]
     @coach=params[:coach]
     @point_guard=params[:point_guard]
     @shooting_guard=params[:shooting_guard]
