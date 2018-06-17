15ae686cf0a8d6d4a45671401b05d03fb1422d81
Karl Amoguis <ktamoguis@gmail.com>
2018-06-17 09:23:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team=params[:team_name]
-    @coach=params[:coach]
-    @point_guard=params[:point_guard]
-    @shooting_guard=params[:shooting_guard]
-    @small_forward=params[:small_forward]
-    @power_forward=params[:power_forward]
-    @center=params[:center]
+    @team = params[:team_name]
+    @coach = params[:coach]
+    @point_guard = params[:point_guard]
+    @shooting_guard = params[:shooting_guard]
+    @small_forward = params[:small_forward]
+    @power_forward = params[:power_forward]
+    @center = params[:center]
 
     erb :team
   end
